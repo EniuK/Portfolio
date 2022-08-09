886ccbc14561cbd44252dc862fc935a78dc674fe
EniuK <108581363+EniuK@users.noreply.github.com>
2022-08-10 00:16:12 +0200

    More goals added

--- src/components/link/goals/goals.jsx
+++ src/components/link/goals/goals.jsx
@@ -4,15 +4,25 @@ const Goals = () => {
     <div>
       <div>
         <h1>GOALS</h1>
-        <h4>
-          My goals for today are pretty simple:
-          <ol>
-            <li>Learn as many programming languages as possible</li>
-            <li>Take 1st place in street lifting</li>
-            <li>Create toxic-free familly {`:)`}</li>
-          </ol>
-        </h4>
+        <h2>My goals for today are pretty simple:</h2>
+        <ol>
+          <li>Learn as many programming languages as possible</li>
+          <li>Take 1st place in street lifting</li>
+          <li>Create toxic-free familly {`:)`}</li>
+        </ol>
       </div>
+      <div>
+        <h2>How am I gonna achive this?</h2>
+        <p>
+          I know from experience that working hard and smart will give you good
+          results but you cant stop doing what you think is right. How i know
+          that? One of many examples is that I simply lost 40kg weight in a bit
+          more than a year. I was also training one of the hardest sports
+          existing - calisthenics. Its a training based on your body weight so
+          it was pretty hard quest to do while you are overweight.
+        </p>
+      </div>
+      <div></div>
     </div>
   );
 };
