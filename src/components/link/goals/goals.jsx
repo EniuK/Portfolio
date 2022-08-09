04c4c35704e0d7981f731731ac157495f9313aa6
EniuK <108581363+EniuK@users.noreply.github.com>
2022-08-10 00:04:41 +0200

    Add goals of life

--- src/components/link/goals/goals.jsx
+++ src/components/link/goals/goals.jsx
@@ -2,7 +2,17 @@ import "./goals.scss";
 const Goals = () => {
   return (
     <div>
-      <p>Goals</p>
+      <div>
+        <h1>GOALS</h1>
+        <h4>
+          My goals for today are pretty simple:
+          <ol>
+            <li>Learn as many programming languages as possible</li>
+            <li>Take 1st place in street lifting</li>
+            <li>Create toxic-free familly {`:)`}</li>
+          </ol>
+        </h4>
+      </div>
     </div>
   );
 };
