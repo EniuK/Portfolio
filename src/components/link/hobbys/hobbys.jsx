06c575e174109dd31bffaf56770b3eb7479abb83
EniuK <108581363+EniuK@users.noreply.github.com>
2022-08-11 22:30:54 +0200

    add hobby

--- src/components/link/hobbys/hobbys.jsx
+++ src/components/link/hobbys/hobbys.jsx
@@ -3,7 +3,13 @@ import "./hobbys.scss";
 const Hobbys = () => {
   return (
     <div className="hobby">
-      <p>Hobby</p>
+      <h1>Hobbys</h1>
+      <h3>Programming</h3>
+      <p>
+        Yes, programming is also my hobby. I really enjoy creating working
+        websites. My github and projects shows that I am commited to become a
+        programmer cause I can also work in my hobby and live for that{" "}
+      </p>
     </div>
   );
 };
