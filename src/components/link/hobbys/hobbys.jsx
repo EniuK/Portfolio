0526904f2b3a693da66856e98e60df52a031a550
EniuK <108581363+EniuK@users.noreply.github.com>
2022-08-18 23:28:20 +0200

    add hobby

--- src/components/link/hobbys/hobbys.jsx
+++ src/components/link/hobbys/hobbys.jsx
@@ -10,6 +10,12 @@ const Hobbys = () => {
         websites. My github and projects shows that I am commited to become a
         programmer cause I can also work in my hobby and live for that{" "}
       </p>
+      <h3>Sport</h3>
+      <p>
+        The second hobby is calisthenics. It's a sport when you exercise with
+        your own bodyweight. Mostly people are small and light but there happens{" "}
+        {"(me etc.)"} people who weight mor ethan usual.
+      </p>
     </div>
   );
 };
